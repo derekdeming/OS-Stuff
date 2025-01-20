6740c8f12a72e3a438d2d27653c7255e0a6f6e0f
derekdeming <derekdeming17@gmail.com>
2025-01-19 22:57:16 -0500

    minimal kernel

--- tinyCOS/run.sh
+++ tinyCOS/run.sh
@@ -4,5 +4,12 @@ set -xue
 # QEMU file path
 QEMU=qemu-system-riscv32
 
+# path to clang and compiler flags
+CC=/opt/homebrew/opt/llvm/bin/clang
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32 -ffreestanding -nostdlib"
+
+# build the kernel 
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c
+
 # Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf

72d04b0f9e4b81e156cf07dfa79c3087aef3accd
Derek Deming <derekdeming17@gmail.com>
2025-09-24 13:23:30 -0400

    process management update

--- tinyCOS/05-06/kernel.h
+++ tinyCOS/05-06/kernel.h
@@ -1,15 +1,84 @@
-#ifndef KERNEL_H
-#define KERNEL_H
+#pragma once
+
+#include "common.h"
+
+#define PROCS_MAX 8
+#define PROC_UNUSED   0
+#define PROC_RUNNABLE 1
+
+#define SCAUSE_ECALL 8
+
+struct process {
+    int pid;
+    int state;
+    vaddr_t sp;
+    uint8_t stack[8192];
+};
 
 struct sbiret {
     long error;
     long value;
 };
 
+struct trap_frame {
+    uint32_t ra;
+    uint32_t gp;
+    uint32_t tp;
+    uint32_t t0;
+    uint32_t t1;
+    uint32_t t2;
+    uint32_t t3;
+    uint32_t t4;
+    uint32_t t5;
+    uint32_t t6;
+    uint32_t a0;
+    uint32_t a1;
+    uint32_t a2;
+    uint32_t a3;
+    uint32_t a4;
+    uint32_t a5;
+    uint32_t a6;
+    uint32_t a7;
+    uint32_t s0;
+    uint32_t s1;
+    uint32_t s2;
+    uint32_t s3;
+    uint32_t s4;
+    uint32_t s5;
+    uint32_t s6;
+    uint32_t s7;
+    uint32_t s8;
+    uint32_t s9;
+    uint32_t s10;
+    uint32_t s11;
+    uint32_t sp;
+};
+
+#define READ_CSR(reg)                                                          \
+    ({                                                                         \
+        unsigned long __tmp;                                                   \
+        __asm__ __volatile__("csrr %0, " #reg : "=r"(__tmp));                  \
+        __tmp;                                                                 \
+    })
+
+#define WRITE_CSR(reg, value)                                                  \
+    do {                                                                       \
+        uint32_t __tmp = (value);                                              \
+        __asm__ __volatile__("csrw " #reg ", %0" ::"r"(__tmp));                \
+    } while (0)
+
+#define PANIC(fmt, ...)                                                        \
+    do {                                                                       \
+        printf("PANIC: %s:%d: " fmt "\n", __FILE__, __LINE__, ##__VA_ARGS__);  \
+        while (1) {}                                                           \
+    } while (0)
+
+struct process *create_process(uint32_t pc);
+void switch_context(uint32_t *prev_sp, uint32_t *next_sp);
+void yield(void);
 struct sbiret sbi_call(long arg0, long arg1, long arg2,
                        long arg3, long arg4, long arg5,
                        long fid, long eid);
 void putchar(char ch);
+void kernel_entry(void);
 void kernel_main(void);
-
-#endif

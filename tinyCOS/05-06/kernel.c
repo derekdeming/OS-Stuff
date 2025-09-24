203b021c4601e15610dbeee0a6ad4d0cb695cf00
Derek Deming <derekdeming17@gmail.com>
2025-09-24 13:24:43 -0400

    new limit scope

--- tinyCOS/05-06/kernel.c
+++ tinyCOS/05-06/kernel.c
@@ -1,9 +1,13 @@
-// kernel.c
 #include "kernel.h"
 #include "common.h"
 
-// Optional externs if you're clearing BSS or referencing linker symbols:
-// extern char __bss[], __bss_end[];
+extern char __bss[], __bss_end[], __stack_top[];
+
+struct process procs[PROCS_MAX];
+struct process *current_proc;
+struct process *idle_proc;
+struct process *proc_a;
+struct process *proc_b;
 
 struct sbiret sbi_call(long arg0, long arg1, long arg2,
                        long arg3, long arg4, long arg5,
@@ -18,47 +22,257 @@ struct sbiret sbi_call(long arg0, long arg1, long arg2,
     register long a6 __asm__("a6") = fid;
     register long a7 __asm__("a7") = eid;
 
-    __asm__ __volatile__ ("ecall"
-        : "=r"(a0), "=r"(a1)
-        : "r"(a0), "r"(a1), "r"(a2),
-          "r"(a3), "r"(a4), "r"(a5),
-          "r"(a6), "r"(a7)
-        : "memory");
+    __asm__ __volatile__("ecall"
+                         : "=r"(a0), "=r"(a1)
+                         : "r"(a0), "r"(a1), "r"(a2), "r"(a3), "r"(a4), "r"(a5),
+                           "r"(a6), "r"(a7)
+                         : "memory");
 
-    struct sbiret ret;
-    ret.error = a0;
-    ret.value = a1;
-    return ret;
+    return (struct sbiret){.error = a0, .value = a1};
 }
 
 void putchar(char ch)
 {
-    // Using fid=1, eid=0 for a legacy console putchar in many SBI specs.
-    sbi_call(ch, 0, 0, 0, 0, 0, 1, 0);
+    sbi_call(ch, 0, 0, 0, 0, 0, 0, 1); // legacy console putchar
+}
+
+static void delay(void)
+{
+    for (int i = 0; i < 30000000; i++)
+        __asm__ __volatile__("nop");
+}
+
+struct process *create_process(uint32_t pc)
+{
+    struct process *proc = NULL;
+
+    for (int i = 0; i < PROCS_MAX; i++) {
+        if (procs[i].state == PROC_UNUSED) {
+            proc = &procs[i];
+            break;
+        }
+    }
+
+    if (!proc)
+        PANIC("no free process slots");
+
+    uint32_t *sp = (uint32_t *) &proc->stack[sizeof(proc->stack)];
+    *--sp = 0;             // s11
+    *--sp = 0;             // s10
+    *--sp = 0;             // s9
+    *--sp = 0;             // s8
+    *--sp = 0;             // s7
+    *--sp = 0;             // s6
+    *--sp = 0;             // s5
+    *--sp = 0;             // s4
+    *--sp = 0;             // s3
+    *--sp = 0;             // s2
+    *--sp = 0;             // s1
+    *--sp = 0;             // s0
+    *--sp = pc;            // ra
+
+    proc->pid = (int) (proc - procs) + 1;
+    proc->state = PROC_RUNNABLE;
+    proc->sp = (uint32_t) sp;
+    return proc;
+}
+
+static struct process *next_runnable(void)
+{
+    struct process *next = idle_proc;
+    for (int i = 0; i < PROCS_MAX; i++) {
+        struct process *proc = &procs[(current_proc->pid + i) % PROCS_MAX];
+        if (proc->state == PROC_RUNNABLE && proc->pid > 0) {
+            next = proc;
+            break;
+        }
+    }
+    return next;
+}
+
+void yield(void)
+{
+    struct process *next = next_runnable();
+    if (next == current_proc)
+        return;
+
+    struct process *prev = current_proc;
+    current_proc = next;
+
+    __asm__ __volatile__(
+        "csrw sscratch, %[sscratch]\n"
+        :
+        : [sscratch] "r" ((uint32_t) &next->stack[sizeof(next->stack)])
+    );
+
+    switch_context(&prev->sp, &next->sp);
+}
+
+void proc_a_entry(void)
+{
+    printf("starting process A\n");
+    while (1) {
+        putchar('A');
+        delay();
+        yield();
+    }
+}
+
+void proc_b_entry(void)
+{
+    printf("starting process B\n");
+    while (1) {
+        putchar('B');
+        delay();
+        yield();
+    }
+}
+
+void handle_trap(struct trap_frame *frame)
+{
+    (void) frame;
+    uint32_t scause = READ_CSR(scause);
+    uint32_t stval = READ_CSR(stval);
+    uint32_t sepc = READ_CSR(sepc);
+    PANIC("unexpected trap scause=%x stval=%x sepc=%x", scause, stval, sepc);
+}
+
+__attribute__((naked))
+__attribute__((aligned(4)))
+void kernel_entry(void)
+{
+    __asm__ __volatile__(
+        "csrrw sp, sscratch, sp\n"
+        "addi sp, sp, -4 * 31\n"
+        "sw ra,  4 * 0(sp)\n"
+        "sw gp,  4 * 1(sp)\n"
+        "sw tp,  4 * 2(sp)\n"
+        "sw t0,  4 * 3(sp)\n"
+        "sw t1,  4 * 4(sp)\n"
+        "sw t2,  4 * 5(sp)\n"
+        "sw t3,  4 * 6(sp)\n"
+        "sw t4,  4 * 7(sp)\n"
+        "sw t5,  4 * 8(sp)\n"
+        "sw t6,  4 * 9(sp)\n"
+        "sw a0,  4 * 10(sp)\n"
+        "sw a1,  4 * 11(sp)\n"
+        "sw a2,  4 * 12(sp)\n"
+        "sw a3,  4 * 13(sp)\n"
+        "sw a4,  4 * 14(sp)\n"
+        "sw a5,  4 * 15(sp)\n"
+        "sw a6,  4 * 16(sp)\n"
+        "sw a7,  4 * 17(sp)\n"
+        "sw s0,  4 * 18(sp)\n"
+        "sw s1,  4 * 19(sp)\n"
+        "sw s2,  4 * 20(sp)\n"
+        "sw s3,  4 * 21(sp)\n"
+        "sw s4,  4 * 22(sp)\n"
+        "sw s5,  4 * 23(sp)\n"
+        "sw s6,  4 * 24(sp)\n"
+        "sw s7,  4 * 25(sp)\n"
+        "sw s8,  4 * 26(sp)\n"
+        "sw s9,  4 * 27(sp)\n"
+        "sw s10, 4 * 28(sp)\n"
+        "sw s11, 4 * 29(sp)\n"
+
+        "csrr a0, sscratch\n"
+        "sw a0,  4 * 30(sp)\n"
+
+        "addi a0, sp, 4 * 31\n"
+        "csrw sscratch, a0\n"
+
+        "mv a0, sp\n"
+        "call handle_trap\n"
+
+        "lw ra,  4 * 0(sp)\n"
+        "lw gp,  4 * 1(sp)\n"
+        "lw tp,  4 * 2(sp)\n"
+        "lw t0,  4 * 3(sp)\n"
+        "lw t1,  4 * 4(sp)\n"
+        "lw t2,  4 * 5(sp)\n"
+        "lw t3,  4 * 6(sp)\n"
+        "lw t4,  4 * 7(sp)\n"
+        "lw t5,  4 * 8(sp)\n"
+        "lw t6,  4 * 9(sp)\n"
+        "lw a0,  4 * 10(sp)\n"
+        "lw a1,  4 * 11(sp)\n"
+        "lw a2,  4 * 12(sp)\n"
+        "lw a3,  4 * 13(sp)\n"
+        "lw a4,  4 * 14(sp)\n"
+        "lw a5,  4 * 15(sp)\n"
+        "lw a6,  4 * 16(sp)\n"
+        "lw a7,  4 * 17(sp)\n"
+        "lw s0,  4 * 18(sp)\n"
+        "lw s1,  4 * 19(sp)\n"
+        "lw s2,  4 * 20(sp)\n"
+        "lw s3,  4 * 21(sp)\n"
+        "lw s4,  4 * 22(sp)\n"
+        "lw s5,  4 * 23(sp)\n"
+        "lw s6,  4 * 24(sp)\n"
+        "lw s7,  4 * 25(sp)\n"
+        "lw s8,  4 * 26(sp)\n"
+        "lw s9,  4 * 27(sp)\n"
+        "lw s10, 4 * 28(sp)\n"
+        "lw s11, 4 * 29(sp)\n"
+        "lw sp,  4 * 30(sp)\n"
+        "sret\n"
+    );
+}
+
+__attribute__((naked))
+void switch_context(uint32_t *prev_sp, uint32_t *next_sp)
+{
+    __asm__ __volatile__(
+        "addi sp, sp, -13 * 4\n"
+        "sw ra,  0  * 4(sp)\n"
+        "sw s0,  1  * 4(sp)\n"
+        "sw s1,  2  * 4(sp)\n"
+        "sw s2,  3  * 4(sp)\n"
+        "sw s3,  4  * 4(sp)\n"
+        "sw s4,  5  * 4(sp)\n"
+        "sw s5,  6  * 4(sp)\n"
+        "sw s6,  7  * 4(sp)\n"
+        "sw s7,  8  * 4(sp)\n"
+        "sw s8,  9  * 4(sp)\n"
+        "sw s9,  10 * 4(sp)\n"
+        "sw s10, 11 * 4(sp)\n"
+        "sw s11, 12 * 4(sp)\n"
+        "sw sp, (a0)\n"
+        "lw sp, (a1)\n"
+        "lw ra,  0  * 4(sp)\n"
+        "lw s0,  1  * 4(sp)\n"
+        "lw s1,  2  * 4(sp)\n"
+        "lw s2,  3  * 4(sp)\n"
+        "lw s3,  4  * 4(sp)\n"
+        "lw s4,  5  * 4(sp)\n"
+        "lw s5,  6  * 4(sp)\n"
+        "lw s6,  7  * 4(sp)\n"
+        "lw s7,  8  * 4(sp)\n"
+        "lw s8,  9  * 4(sp)\n"
+        "lw s9,  10 * 4(sp)\n"
+        "lw s10, 11 * 4(sp)\n"
+        "lw s11, 12 * 4(sp)\n"
+        "addi sp, sp, 13 * 4\n"
+        "ret\n"
+    );
 }
 
 void kernel_main(void)
 {
-    // If using global variables in .bss, zero it out (optional):
-    // for (char *b = __bss; b < __bss_end; b++) {
-    //     *b = 0;
-    // }
+    memset(__bss, 0, (size_t) __bss_end - (size_t) __bss);
 
-    const char *hello = "Hello, World!\n";
-    printf("%s", hello);
-    printf("1 + 2 = %d, %x\n", 1 + 2, 0x1234abcd);
-    // for (const char *p = hello; *p != '\0'; p++) {
-    //     putchar(*p);
-    // }
+    printf("\n\n");
 
-    // Halt/idle forever
-    while (1) {
-        __asm__ __volatile__("wfi");
-    }
+    WRITE_CSR(stvec, (uint32_t) kernel_entry);
+
+    idle_proc = create_process(0);
+    idle_proc->pid = 0;
+    current_proc = idle_proc;
+
+    proc_a = create_process((uint32_t) proc_a_entry);
+    proc_b = create_process((uint32_t) proc_b_entry);
+
+    yield();
+
+    PANIC("switched to idle process");
 }
-
-// sbi_call is a function that makes an SBI call to the RISC-V platform.
-// It takes a function ID (fid) and an extension ID (eid) as arguments (a6 being the fid and a7 being the eid)
-// It returns a struct sbiret containing the error and value of the SBI call.
-// we need to make sure all registers except a0 and a1 are preserved because a0 and a1 are used for the returning an error and a1 returning the return values of the SBI call. Ultimately, we need to make sure that the memory is not modified.
-// the __asm__ __volatile__ is used to tell the compiler that the assembly code is volatile and that the compiler should not optimize it.
